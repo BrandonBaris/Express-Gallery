56cda44d8abfd070ad6ac2e4ee59796c596ed31a
bradbaris <brad@nomadspaceship.com>
2015-03-05 16:50:32 -1000

    starting mq css

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
-app.use( express.static( __dirname + './app') );
+app.use( express.static( __dirname + '/app') );
 app.set( 'view engine', 'jade' );
 
 var mongoose = require('mongoose');
