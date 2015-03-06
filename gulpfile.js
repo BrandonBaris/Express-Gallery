baaa1417f78f61df36433f520e87bb0dc3d17aea
bradbaris <brad@nomadspaceship.com>
2015-03-05 15:58:40 -1000

    moving fs around

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var connect = require('gulp-connect');
 var jade = require('gulp-jade');
+var server = require( 'gulp-express');
 
 gulp.task('connect', function() {
   connect.server({
@@ -9,6 +10,16 @@ gulp.task('connect', function() {
       port: 8080,
       livereload: true
   });
+});
+
+gulp.task('server', function() {
+  server.run(['app.js']);
+  
+  gulp.watch('./sass/**/*.scss', [server.notify]);
+  gulp.watch('./views/**/*.jade', [server.notify]);
+  // have no idea if below line works
+  gulp.watch('./app/js/app.js', [server.run(['app.js'])]);
+
 });
 
 gulp.task('sass', function() {
@@ -39,5 +50,6 @@ gulp.task('watch', function() {
   gulp.watch('./app/**/*', ['livereload']);
 });
 
-gulp.task('default', ['connect','watch','sass']);
+gulp.task('default', ['server','watch','sass']);
+gulp.task('connect', ['connect','watch','sass']);
 gulp.task('no-server', ['watch','sass']);
