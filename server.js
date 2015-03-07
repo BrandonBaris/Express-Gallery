d8146b72d7d00692dc8e9c8168d5b90fe182cd1a
bradbaris <brad@nomadspaceship.com>
2015-03-06 15:56:44 -1000

    WIP. added views/auth

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
 var methodOverride = require('method-override');
-var gallery = require('./controllers/gallery');
 var mongoose = require('mongoose');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(methodOverride('_method'));
+var gallery = require('./controllers/gallery');
+var auth = require('./controllers/auth');
+
+var app = express();
+
 app.use( express.static( __dirname + '/app') );
+
+app.use(session({ secret: 'keyboard cat' }));
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.set('views', __dirname + '/views');
 app.set( 'view engine', 'jade' );
 
 mongoose.connect('mongodb://localhost/photos');
 
+app.use( auth );
 app.use( '/gallery', gallery );
 
 // render main gallery
